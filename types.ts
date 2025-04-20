135c8c0be50b032701761c95c5aba639868e4d86
SenSe1 <102629374+Sen-Se1@users.noreply.github.com>
2025-04-20 23:18:04 +0100

    init

---
+++ types.ts
@@ -0,0 +1,23 @@
+import { Card, List } from "@prisma/client";
+
+export type ListWithCards = List & { cards: Card[] };
+
+export type CardWithList = Card & { list: List };
+
+export type Plan = {
+  id: string;
+  name: string;
+  price: number;
+  features: string[];
+  description: string;
+  maxUsers: number;
+  maxWorkspaces: number;
+  maxStorage: number;
+};
+
+export type EmailPayload = {
+  from: string;
+  to: string;
+  subject: string;
+  html: string;
+};
